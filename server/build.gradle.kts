cec2cf4e66e439debd8ecf46e3b25d64599c7c75
fenleon <fenleon@users.noreply.github.com>
2026-08-17 15:25:39 +0200

    0.6.1: version bump

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.lightphone.audiobooks.server"
         minSdk = 34
         targetSdk = 36
-        versionCode = 8
-        versionName = "0.5.7"
+        versionCode = 9
+        versionName = "0.5.8"
     }
 
     buildTypes {
